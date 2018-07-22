b466fe67abff6adb8cc9972d25fe74c0a6d23c75
jatintiwari <mail@jatintiwari.com>
2018-07-22 10:34:43 +0530

    rx4

--- rx4.jsx
+++ rx4.jsx
@@ -1,7 +1,7 @@
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 
-const subscription = Observable.create((observer) => {
+const observable = Observable.create((observer) => {
   let i = 0;
   let interval = setInterval(() => observer.next(++i), 1000);
 
@@ -11,9 +11,9 @@ const subscription = Observable.create((observer) => {
 });
 
 
-subscription.subscribe((data) => console.log('obserevable [data]', data));
+const subscription = observable.subscribe((data) => console.log('obserevable [data]', data));
 
 const subject = new Subject();
 let j = 0;
 subject.subscribe((data) => console.log('[subject] data', data));
-setInterval(() => subject.next(j+=1), 1000);
+setInterval(() => subject.next(j += 1), 1000);
