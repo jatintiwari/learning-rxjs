87c6b61951a4d3ec4d109aa828234a10c23c3916
jatintiwari <mail@jatintiwari.com>
2018-07-22 10:25:36 +0530

    add/cancell multiple subscriptions

--- rx5.jsx
+++ rx5.jsx
@@ -1,19 +1,33 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/dom/ajax';
 
-
-const subscription = Observable.ajax({
+const observable = Observable.ajax({
   url: 'http://localhost:3000/home/delay10',
   progressSubscriber: {
     complete: () => console.log('completed'),
     error: (error) => console.error('[subcription]', error)
-  },
-
+  }
 });
 
-const subscriber = subscription.subscribe((xhr) => console.log('[subscription]', xhr.response));
+const observable2 = Observable.ajax({
+  url: 'http://localhost:3000/home/delay10',
+  progressSubscriber: {
+    complete: () => console.log('completed'),
+    error: (error) => console.error('[subcription]', error)
+  }
+});
+
+
+const subscription = observable.subscribe((xhr) => console.log('[subscription]', xhr.response));
+const subscription2 = observable2.subscribe((xhr) => console.log('[subscription]', xhr.response));
+
+/**
+ * adding a subscription to another.
+ * Cancelling 1 will cancel both
+ */
+subscription.add(subscription2);
 
 setTimeout(() => {
   console.log('[unsubscribe] completing ...');
-  // subscriber.complete()
+  subscription.unsubscribe()
 }, 3000);
